f2e7df603f4672e85dd25894d87351172856200a
Milad Mirzaei <m.mirzaei@alibaba.ir>
2025-12-26 14:54:19 +0330

    feat: add mockoon env and get the data and test it

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
   plugins: [reactRouter(), tsconfigPaths()],
 });
